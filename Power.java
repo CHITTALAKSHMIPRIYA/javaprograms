941449d488e47b9012e854c95281a2bd5df6b219
CHITTALAKSHMIPRIYA <lakshmichitta96@gmail.com>
2018-05-15 15:09:45 +0530

    power

---
+++ Power.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Power
+{
+public static int pwr(int num,int p)
+{
+int res=1
+for(int i=1;i<=p;i++)
+{
+res=num*res;
+}
+return res;
+}
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the num");
+int num=sc.nextInt();
+System.out.println("enter pwr");
+int p=sc.nextInt();
+int res=pwr(num,p);
+System.out.println(res);
+}
+} 
